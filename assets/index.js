5c069511ed1e039395b9cdc9374d0f62f4cba80c
tolulopzy <christabelodeyemi@gmail.com>
2021-09-30 23:02:25 +0100

    feat: creation of DevBooks

---
+++ assets/index.js
@@ -0,0 +1,40 @@
+//  Javascript for toggle menu
+var MenuItems = document.getElementById("MenuItems");
+MenuItems.style.maxHeight = "0px";
+function menutoggle() {
+if (MenuItems.style.maxHeight == "0px") {
+    MenuItems.style.maxHeight = "200px";
+} else {
+    MenuItems.style.maxHeight = "0px";
+}
+}
+//toggle form
+var signInForm = document.getElementById("signInForm");
+    var signUpForm = document.getElementById("signUpForm");
+    var indicator = document.getElementById("indicator");
+
+    function signIn() {
+        signUpForm.style.transform = "translateX(300px)";
+        signInForm.style.transform = "translateX(300px)";
+        indicator.style.transform = "translateX(0px)";
+    }
+    function signUp() {
+        signUpForm.style.transform = "translateX(0px)";
+        signInForm.style.transform = "translateX(0px)";
+        indicator.style.transform = "translateX(100px)";
+    }
+    // form validation
+    function formvalidate() {
+        var ptrn = /^([^0-9\W]*)$/;
+        if (ptrn.test(document.myform.uname.value)) {
+        document.getElementById("uname").textContent = "User Name is Valid";
+        document.getElementById("uname").style.background = "#72EF57";
+        document.getElementById("uname").style.fontSize = "12px";
+        } else {
+        document.getElementById("uname").textContent = "User Name is Invalid";
+        document.getElementById("uname").style.background = "#EF6257";
+        document.getElementById("uname").style.fontSize = "12px";
+        }
+    }
+
+    document.myform.uname.addEventListener("blur", formvalidate);
